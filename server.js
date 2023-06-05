8975d16d882f9f39ac5ad7a4ab4d5f4514b6273e
Nolan Knill <nolanknill@gmail.com>
2023-06-05 11:37:10 -0400

    Adds server.js and express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+// basic home route
+app.get('/', (req, res) => {
+  res.send('Welcome to my API');
+});
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
